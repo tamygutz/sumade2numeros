0ef4eeed1900b564acf7ffdb77b924381f8ba294
tamygutz <sarai2008ixt@live.com.mx>
2022-03-18 12:31:46 -0600

    codigo del programa suma de dos números enteros

---
+++ repos python/sumade2numeros.py
@@ -0,0 +1,17 @@
+##archivo: suma de 2 numeros.py 
+##autor: Tamara Sarai Gutierrez Flores 
+##fecha: 16-03-2022
+##descripcion: este programa suma 2 numeros enteros ingresados por el usuario
+
+num1=0
+num2=0
+suma=0
+
+print("Bienvenido usuario")
+print("Ingrese un numero entero")
+num1=int(input())
+
+print("Ingrese otro numero entero")
+num2=int(input())
+suma=num1+num2
+print("El resultado de la suma es:" , suma)
